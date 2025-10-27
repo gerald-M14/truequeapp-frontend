1d2da4268683810ff9728b4b45591c1f8e381e64
gerald-M14 <geraldhymessu7@gmail.com>
2025-10-27 03:06:08 -0500

    Subiendo archivos

--- src/components/logout.jsx
+++ src/components/logout.jsx
@@ -4,7 +4,7 @@ import { useAuth0 } from '@auth0/auth0-react'
 export const LogoutButton = () => {
   const { logout } = useAuth0()
   return (
-    <button className="px-4 py-2 bg-teal-600 cursor-pointer text-white text-sm rounded-lg hover:bg-teal-900 transition" onClick={() => logout({ logoutParams: { returnTo: import.meta.env.VITE_AUTH0_REDIRECT_URI } })}>
+    <button className="px-4 py-2 bg-teal-600 cursor-pointer text-white text-sm rounded-lg hover:bg-teal-900 transition" onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
       Cerrar sesión
     </button>
   )
